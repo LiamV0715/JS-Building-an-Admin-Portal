08f65eebd147ef9d44dc52fe9bd1385cf2eeb4a8
Liam Vermette <Liam.vermette@gmail.com>
2024-03-18 21:09:56 -0400

    Admin Portal Assignment

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,60 @@
+async function main() {
+    const response = await fetch('http://localhost:3001/listBooks')
+    const data = await response.json()
+    data.forEach(book => renderInput(book))
+}
 
-// Your Code Here
+ 
+
+function renderInput(book) {
+    const root = document.getElementById('root')
+    const li = document.createElement('li')
+
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveBtn = document.createElement('button')
+    saveBtn.textContent = 'Save'
+
+    function renderInput(book) {
+
+        const root = document.getElementById('root')
+        const li = document.createElement('li')
+    
+        li.textContent = book.title
+        const input = document.createElement('input')
+    
+        input.value = book.quantity
+        const saveBtn = document.createElement('button')
+    
+        saveBtn.textContent = 'Save'
+        saveBtn.addEventListener('click', async function() {
+            await fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+    
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: input.value
+                })
+            })
+        })
+    
+     
+        li.append(input, saveBtn)
+        root.append(li)
+    
+    }
+
+    li.append(input, saveBtn)
+    root.append(li)
+}
+
+ 
+
+main()
+
