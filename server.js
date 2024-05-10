6064b69ea1621b6efbb97efb675e3ff6f08e3986
Liam Vermette <Liam.vermette@gmail.com>
2024-05-10 16:56:37 -0400

    debugged get route

--- server.js
+++ server.js
@@ -19,20 +19,16 @@ async function main() {
   app.patch("/updateBook", async (req, res) => {
     let books = await loadBooks();
     if (!req.body.id)
-      return res
-        .status(400)
-        .json({
-          error: true,
-          message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
-        });
+      return res.status(400).json({
+        error: true,
+        message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
+      });
     let book = books.find((book) => book.id === req.body.id);
     if (!book)
-      return res
-        .status(404)
-        .json({
-          error: true,
-          message: `Could not find a book with an id of ${req.body.id}`,
-        });
+      return res.status(404).json({
+        error: true,
+        message: `Could not find a book with an id of ${req.body.id}`,
+      });
     const { title, year, quantity, imageURL, description } = {
       ...book,
       ...req.body,
@@ -45,26 +41,20 @@ async function main() {
   app.post("/addBook", async (req, res) => {
     let books = await loadBooks();
     if (!req.body.title)
-      return res
-        .status(400)
-        .json({
-          error: true,
-          message: `'title' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
-        });
+      return res.status(400).json({
+        error: true,
+        message: `'title' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
+      });
     if (!req.body.quantity)
-      return res
-        .status(400)
-        .json({
-          error: true,
-          message: `'quantity' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
-        });
+      return res.status(400).json({
+        error: true,
+        message: `'quantity' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
+      });
     if (!req.body.description)
-      return res
-        .status(400)
-        .json({
-          error: true,
-          message: `'description' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
-        });
+      return res.status(400).json({
+        error: true,
+        message: `'description' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
+      });
 
     const { title, year, quantity, imageURL, description } = req.body;
     const id = books.reduce((id, book) => Math.max(book.id + 1, id), 1);
@@ -77,28 +67,24 @@ async function main() {
   app.delete("/removeBook/:id", async (req, res) => {
     let books = await loadBooks();
     if (!req.params.id)
-      return res
-        .status(400)
-        .json({
-          error: true,
-          message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
-        });
+      return res.status(400).json({
+        error: true,
+        message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`,
+      });
     let bookToDelete = books.find(
       (book) => book.id === parseInt(req.params.id)
     );
     if (!bookToDelete)
-      return res
-        .status(404)
-        .json({
-          error: true,
-          message: `Could not find a book with an id of ${req.body.id}`,
-        });
+      return res.status(404).json({
+        error: true,
+        message: `Could not find a book with an id of ${req.body.id}`,
+      });
     books = books.filter((book) => book !== bookToDelete);
     await saveBooks(books);
     res.json(bookToDelete);
   });
 
-  app.listen(3001, () => {
+  app.listen(3000, () => {
     liveServer.start({
       port: 3000,
       logLevel: 0,
